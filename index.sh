8009392784b77f3713681ffebf982580b84a1fc8
tuanngocptn <tuanngocptn@gmail.com>
2023-06-07 16:01:35 +0700

    build project

--- index.sh
+++ index.sh
@@ -1,3 +1,6 @@
+panda_health_check() {
+  echo "Panda is Ok"
+}
 get_funny_idiom() {
   RANDOM_FUNNY_IDIOMS_GIST_URL=https://gist.githubusercontent.com/tuanngocptn/9fabb1979e5b29eca84bce28c6b8d080/raw/random-idioms-vi.sh
   RESULT="$(bash -c "$(curl -fsSL ${RANDOM_FUNNY_IDIOMS_GIST_URL})")"
