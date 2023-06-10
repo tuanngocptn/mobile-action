25cf1487fa62c8eaa308d1be0c2ffe4135ab2cbd
tuanngocptn <tuanngocptn@gmail.com>
2023-06-11 02:37:32 +0700

    add panda for push other chanel

--- index.sh
+++ index.sh
@@ -23,6 +23,33 @@ get_message_information() {
   echo $MESSAGE_INFO
 }
 send_telegram() {
+  DATA_PUSH=''
+  if [ "${2}" ]; then
+    if [ "${3}" ]; then
+      DATA_PUSH='{
+        "chat_id": "'"$1"'",
+        "reply_to_message_id": "'"$2"'",
+        "text": "'"$3"'"
+      }'
+    else
+      DATA_PUSH='{
+        "chat_id": "'"$1"'",
+        "text": "'"$2"'"
+      }'
+    fi
+    send_telegram_normal $DATA_PUSH
+  else
+    send_telegram_liberty $1
+  fi
+}
+
+send_telegram_normal() {
+  curl --location "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+    --header 'Content-Type: application/json' \
+    --data $1
+}
+
+send_telegram_liberty() {
   curl --location "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
     --header 'Content-Type: application/json' \
     --data '{
